1ee95699905e871ea0e89b4ee6ac6851eceabdce
f.fathollahi <f.fathollahi@DEV-36>
2020-09-03 14:47:57 +0430

    change ApiResource and configuration

---
+++ Plus.Infrastructure.IdentityServer/wwwroot/js/modal.js
@@ -0,0 +1,25 @@
+﻿
+$(function () {
+    var PlaceHolderElement = $('#PlaceHolderHere');
+    $('button[data-toggle="ajax-modal"]').click(function (event) {
+        var url = $(this).data('url');
+        $.get(url).done(function (data) {
+            PlaceHolderElement.html(data);
+            PlaceHolderElement.find('.modal').modal('show');
+        })
+    })
+
+    PlaceHolderElement.on('click', '[data-save="modal"]', function (event) {
+        PlaceHolderElement.find('.modal').modal('hide');
+
+        var form = $(this).children('.modal').find('form');
+        var actionUrl = form.attr('action');
+        var sendData = form.serialize();
+        $.post(actionUrl, sendData).done(function (data) {
+            $("#scopeList").append(JSON.parse(data));
+          //  $('#scopeList').val(sendData);
+           // $("scopeList").html(response);
+            
+        })
+    })
+})
